49ef7e5d88cd01afed5c81227b1bcc7e3123ac39
eliran gabay <34740857+EliranGabay@users.noreply.github.com>
2021-10-21 11:29:04 +0300

    update func

--- src/api.js
+++ src/api.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const sendEmail = require("./sendMail.js");
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!",
-  });
+router.post("/msg", (req, res) => {
+  sendEmail(req, res);
 });
 
 app.use(`/.netlify/functions/api`, router);
